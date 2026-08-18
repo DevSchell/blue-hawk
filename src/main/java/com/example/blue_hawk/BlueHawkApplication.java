9e239f92b673828926d7c18e2026fb9c9cb9c234
Everton Mikey Litka Goncalves <evertonlitka@MacBook-Air-de-Everton.local>
2026-08-18 19:37:26 -0300

    First commit

---
+++ src/main/java/com/example/blue_hawk/BlueHawkApplication.java
@@ -0,0 +1,13 @@
+package com.example.blue_hawk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlueHawkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BlueHawkApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/blue_hawk/BlueHawkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.blue_hawk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlueHawkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
